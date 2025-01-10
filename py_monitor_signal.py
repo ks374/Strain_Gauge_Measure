835befcb67d79b6ddb84ca68247f48015b450141
Chenghang <35774140+ks374@users.noreply.github.com>
2025-01-10 11:11:21 -0800

    Update py_monitor_signal.py

--- py_monitor_signal.py
+++ py_monitor_signal.py
@@ -1,8 +1,12 @@
 import pygame
 import sys
+import time
 
-def show_on_second_monitor(screen,color_code):
-    # Fill the screen with green
+def show_on_second_monitor(screen,color_code,resume_time):
+    green_color = (0,255,0)
     screen.fill(color_code)
-    pygame.display.update()
+    pygame.display.flip()
+    time.sleep(resume_time)
+    screen.fill(green_color)
+    pygame.display.flip()
     
